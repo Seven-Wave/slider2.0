1f8e4b40420aed677999cc9bd76187b1c5becec1
Andrey <58787957+Seven-Wave@users.noreply.github.com>
2021-04-28 17:44:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function (){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        // adaptiveHeight: true,
+    })
+    $(".title__button").click(function(e){
+        e.preventDefault()
+        slideIndex = $(this).attr('data-slick-index')
+        console.log(parseInt(slideIndex))
+        let slider = $('.slider')
+        slider[0].slick.slickGoTo( parseInt(slideIndex) )
+    })
+
+    $(".back_to_title_button").click(function(e){
+        let slider = $('.slider')
+        slider[0].slick.slickGoTo(0)
+    })
+
+    $('.slider__card').click(function () {
+        $('.overview').css({
+            'transform': 'translateY(0)'
+        })
+    })
+
+
+    $('.close').click(function () {
+        $('.overview').css({
+            'transform': 'translateY(100%)'
+        })
+    })
+
+
+})
